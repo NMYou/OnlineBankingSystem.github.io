08a45f4e7a94a9372fd1ebae85c0c3a8de5257fd
NMYou <77392610+NMYou@users.noreply.github.com>
2021-03-05 02:57:30 +0530

    database updated

--- process/database.php
+++ process/database.php
@@ -1,12 +1,15 @@
 <?php
 //Development Connection
-$servername = "localhost";
-$dBUsername = "root";
-$dbPassword = "";
-$dBName = "onlinebanking";
+// $servername = "localhost";
+// $dBUsername = "root";
+// $dbPassword = "";
+// $dBName = "onlinebanking";
 
 //Remote Database Connection
-
+$servername = "remotemysql.com";
+$dBUsername = "eHRMvr2Cvq";
+$dbPassword = "7FuEVESBrO";
+$dBName = "eHRMvr2Cvq";
 
 $conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
 
